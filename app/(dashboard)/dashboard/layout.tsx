b9570b57cb13fec2e608cee63426044bfb5a48be
AchiUgulava <achiugulava@gmail.com>
2024-12-14 02:35:42 +0400

    chore: refactor

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { Users, Settings, Shield, Activity, Menu } from "lucide-react";
+import Header from "@/components/sections/Header";
 
 async function fetchTeamsForUser() {
   const response = await fetch("/api/teams");
@@ -38,70 +39,75 @@ export default function DashboardLayout({
   ];
 
   return (
-    <div className="flex flex-col min-h-[calc(100dvh-68px)] mx-auto w-full">
-      {/* Mobile header */}
-      <div className="lg:hidden flex items-center justify-between bg-white border-b border-gray-200 p-4">
-        <div className="flex items-center">
-          <span className="font-medium">Settings</span>
+    <section className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex flex-col min-h-[calc(100dvh-68px)] mx-auto w-full">
+        {/* Mobile header */}
+        <div className="lg:hidden flex items-center justify-between bg-white border-b border-gray-200 p-4">
+          <div className="flex items-center">
+            <span className="font-medium">Settings</span>
+          </div>
+          <Button
+            className="-mr-3"
+            variant="ghost"
+            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          >
+            <Menu className="h-6 w-6" />
+            <span className="sr-only">Toggle sidebar</span>
+          </Button>
         </div>
-        <Button
-          className="-mr-3"
-          variant="ghost"
-          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-        >
-          <Menu className="h-6 w-6" />
-          <span className="sr-only">Toggle sidebar</span>
-        </Button>
-      </div>
 
-      <div className="flex flex-1 overflow-hidden h-full">
-        {/* Sidebar */}
-        <aside
-          className={`w-64 bg-white lg:bg-gray-50 border-r border-gray-200 lg:block ${
-            isSidebarOpen ? "block" : "hidden"
-          } lg:relative absolute inset-y-0 left-0 z-40 transform transition-transform duration-300 ease-in-out lg:translate-x-0 ${
-            isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-          }`}
-        >
-          <nav className="h-full overflow-y-auto overflow-x-hidden p-4">
-            {navItems.map((item) => (
-              <div key={item.href}>
-                <Link href={item.href} passHref>
-                  <Button
-                    variant={pathname === item.href ? "secondary" : "ghost"}
-                    className={`my-1 w-full justify-start ${
-                      pathname === item.href ? "bg-gray-100" : ""
-                    }`}
-                    onClick={() => setIsSidebarOpen(false)}
-                  >
-                    <item.icon className="mr-2 h-4 w-4" />
-                    {item.label}
-                  </Button>
-                </Link>
-                {/* Render sub-items (team names) */}
-                {item.subItems &&
-                  // !isLoading &&
-                  item.subItems.map((team: any) => (
-                    <Link
-                      key={team.teamId}
-                      href={`/dashboard/team/${team.teamId}`}
+        <div className="flex flex-1 overflow-hidden h-full">
+          {/* Sidebar */}
+          <aside
+            className={`w-64 bg-white lg:bg-gray-50 border-r border-gray-200 lg:block ${
+              isSidebarOpen ? "block" : "hidden"
+            } lg:relative absolute inset-y-0 left-0 z-40 transform transition-transform duration-300 ease-in-out lg:translate-x-0 ${
+              isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+            }`}
+          >
+            <nav className="h-full overflow-y-auto overflow-x-hidden p-4">
+              {navItems.map((item) => (
+                <div key={item.href}>
+                  <Link href={item.href} passHref>
+                    <Button
+                      variant={pathname === item.href ? "secondary" : "ghost"}
+                      className={`my-1 w-full justify-start ${
+                        pathname === item.href ? "bg-gray-100" : ""
+                      }`}
+                      onClick={() => setIsSidebarOpen(false)}
                     >
-                      <Button
-                        variant="ghost"
-                        className="ml-8 my-1 w-full justify-start"
+                      <item.icon className="mr-2 h-4 w-4" />
+                      {item.label}
+                    </Button>
+                  </Link>
+                  {/* Render sub-items (team names) */}
+                  {item.subItems &&
+                    // !isLoading &&
+                    item.subItems.map((team: any) => (
+                      <Link
+                        key={team.teamId}
+                        href={`/dashboard/team/${team.teamId}`}
                       >
-                        {team.name}
-                      </Button>
-                    </Link>
-                  ))}
-              </div>
-            ))}
-          </nav>
-        </aside>
+                        <Button
+                          variant="ghost"
+                          className="ml-8 my-1 w-full justify-start"
+                        >
+                          {team.name}
+                        </Button>
+                      </Link>
+                    ))}
+                </div>
+              ))}
+            </nav>
+          </aside>
 
-        {/* Main content */}
-        <main className="flex-1 overflow-y-auto p-0 lg:p-4 max-w-7xl mx-auto">{children}</main>
+          {/* Main content */}
+          <main className="flex-1 overflow-y-auto p-0 lg:p-4 max-w-7xl mx-auto">
+            {children}
+          </main>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
---
+++ components/sections/Header.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Home, LogOut, LucideTable2 } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useUser } from "@/lib/auth";
+import { signOut } from "@/app/(login)/actions";
+import { useRouter } from "next/navigation";
+import { useQueryClient } from "@tanstack/react-query";
+
+export default function  Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const queryClient = useQueryClient();
+  const { user, setUser } = useUser();
+  const router = useRouter();
+
+  async function handleSignOut() {
+    setUser(null);
+    await signOut();
+    queryClient.clear();
+    router.push("/");
+  }
+
+  return (
+    <header className="border-b border-gray-200">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
+        <Link href="/" className="flex items-center">
+          <LucideTable2 className="h-6 w-6 text-purple-600" />
+          <span className="ml-2 text-xl font-semibold text-gray-900">
+            Tablify Demo
+          </span>
+        </Link>
+        <div className="flex items-center space-x-4">
+          {user ? (
+            <>
+              <Link
+                href="/dashboard"
+                className="text-sm font-medium text-gray-700 hover:text-gray-900"
+              >
+                Dashboard
+              </Link>
+              <DropdownMenu open={isMenuOpen} onOpenChange={setIsMenuOpen}>
+                <DropdownMenuTrigger asChild>
+                  <Avatar className="cursor-pointer size-9">
+                    <AvatarImage alt={user.name || ""} />
+                    <AvatarFallback>
+                      {user.email
+                        .split(" ")
+                        .map((n) => n[0])
+                        .join("")}
+                    </AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  align="end"
+                  className="flex flex-col gap-1"
+                >
+                  <DropdownMenuItem className="cursor-pointer">
+                    <Link
+                      href="/dashboard"
+                      className="flex w-full items-center"
+                    >
+                      <Home className="mr-2 h-4 w-4" />
+                      <span>Dashboard</span>
+                    </Link>
+                  </DropdownMenuItem>
+                  <form action={handleSignOut} className="w-full">
+                    <button type="submit" className="flex w-full">
+                      <DropdownMenuItem className="w-full flex-1 cursor-pointer">
+                        <LogOut className="mr-2 h-4 w-4" />
+                        <span>Sign out</span>
+                      </DropdownMenuItem>
+                    </button>
+                  </form>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          ) : (
+            <>
+              <Link
+                href="/pricing"
+                className="text-sm font-medium text-gray-700 hover:text-gray-900"
+              >
+                Pricing
+              </Link>
+              <Button
+                asChild
+                className="bg-black hover:bg-gray-800 text-white text-sm px-4 py-2 rounded-full"
+              >
+                <Link href="/sign-up">Sign Up</Link>
+              </Button>
+            </>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+}
