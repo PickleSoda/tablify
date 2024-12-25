059c9e81206ae76999102cada07a6c0ec8eeba9a
AchiUgulava <achiugulava@gmail.com>
2024-12-26 02:05:13 +0400

    fix: sync dunamic api bug

--- app/dashboard/team/[teamId]/page.tsx
+++ app/dashboard/team/[teamId]/page.tsx
@@ -8,18 +8,18 @@ interface SettingsPageProps {
 
 export default async function SettingsPage({ params }: SettingsPageProps) {
   const user = await getUser();
-
+  const { teamId } = await params
   if (!user) {
     redirect('/sign-in'); // Redirect if user is not authenticated
   }
 
-  const teamId = parseInt(params.teamId, 10); // Extract and parse the teamId
+  const id = parseInt(teamId, 10); // Extract and parse the teamId
 
-  if (isNaN(teamId)) {
+  if (isNaN(id)) {
     throw new Error('Invalid Team ID'); // Handle invalid teamId
   }
 
-  const teamData = await getTeamForUserByTeamId(user.id, teamId);
+  const teamData = await getTeamForUserByTeamId(user.id, id);
 
   if (!teamData) {
     throw new Error('Team not found');
